c45f2bdb738a39760bed63991066d9b3048093a2
Tianyi Zhan <tianyi.zhan@outlook.com>
2022-10-22 10:13:27 +0800

    add tabBar to the homeview

---
+++ Weather/views/navigation/tabBar.swift
@@ -0,0 +1,40 @@
+//
+//  tabBar.swift
+//  Weather
+//
+//  Created by Tianyi
+//
+
+import SwiftUI
+
+struct tabBar: View {
+    var action : () ->Void
+    var body: some View {
+        ZStack{
+            //MARK: button to navigate to the weather screen
+            HStack{
+                //MARK: button to expand to the home screen
+                // Button(action: {}, label: {})
+                Button {
+                    
+                } label: {
+                    Image(systemName: "mappin.and.ellipse").frame(width: 44, height: 44, alignment: .center)
+                }
+                Spacer()
+                //MARK: navigation button the navigate to different weather
+                // NavigationLink(destination: <#T##() -> _#>, label: <#T##() -> _#>)
+                NavigationLink {
+                    
+                } label: {
+                    Image(systemName: "list.star").frame(width: 44, height: 44)
+                }
+            }.font(.title2).foregroundColor(.white).padding(EdgeInsets(top: 20, leading: 32, bottom: 24, trailing: 32))
+        }.frame(maxHeight: .infinity, alignment: .bottom).ignoresSafeArea()
+    }
+}
+
+struct tabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        tabBar(action: {}).preferredColorScheme(.dark)
+    }
+}
