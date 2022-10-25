401ce8c6e2f12b3bfc622c858e292babe1c460e8
Tianyi Zhan <tianyi.zhan@outlook.com>
2022-10-25 22:13:17 +0800

    add the bottom sheet for weather

---
+++ Weather/views/detail/forecastView.swift
@@ -0,0 +1,20 @@
+//
+//  forecastView.swift
+//  Weather
+//
+//  Created by toyika on 2023/2/5.
+//
+
+import SwiftUI
+
+struct forecastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct forecastView_Previews: PreviewProvider {
+    static var previews: some View {
+        forecastView()
+    }
+}
